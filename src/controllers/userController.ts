bf5c18304b1a700768a61b4e8728c4ac2abd208f
SanjayM-2002 <sanjaymshivakripa@gmail.com>
2024-07-05 22:42:47 +0530

    added login api

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -57,4 +57,42 @@ const signup = async (req: Request, res: Response) => {
   }
 };
 
-export { signup };
+const login = async (req: Request, res: Response) => {
+  const loginBody = req.body;
+  try {
+    console.log('input body is: ', loginBody);
+    const zodResponse = loginInput.safeParse(loginBody);
+    if (!zodResponse.success) {
+      return res.status(411).json(zodResponse.error.message);
+    }
+    const user = await User.findOne({ email: zodResponse.data.email });
+    if (!user) {
+      return res.status(400).json({ error: 'No user with given email exists' });
+    }
+    console.log('user password is: ', user.password);
+    console.log('input password is: ', zodResponse.data.password);
+    const isPasswordCorrect = await bcrypt.compare(
+      zodResponse.data.password,
+      user.password
+    );
+    console.log('Is password correct:', isPasswordCorrect);
+
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ error: 'Invalid password' });
+    }
+    generateToken(user._id.toString(), res);
+    return res.status(200).json({
+      _id: user._id,
+      fullname: user.fullname,
+      email: user.email,
+      message: 'Logged in successfully',
+    });
+  } catch (error) {
+    console.log('Error caught is: ', error.message);
+    return res
+      .status(500)
+      .json({ error: 'Server error', errorDetails: error.message });
+  }
+};
+
+export { signup, login };
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { signup } from '../controllers/userController.js';
+import { signup, login } from '../controllers/userController.js';
 const router = express.Router();
 router.get('/hello', (req, res) => {
   res.status(200).json({ message: 'Hello from user' });
 });
 
 router.post('/signup', signup);
+router.post('/login', login);
 
 export default router;
